e8cd0e8f382393b733f6e15ff7e1f86455c98b5a
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-02-24 13:42:51 +0200

    Changed movement logic

--- Assets/_Scripts/Core/PlayerMovement.cs
+++ Assets/_Scripts/Core/PlayerMovement.cs
@@ -4,14 +4,24 @@ namespace KingOfGuns.Core
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] private float _speed;
+        [SerializeField] private float _maxSpeed;
+        [SerializeField] private float _acceleration;
+        [SerializeField] private float _decceleration;
+        [SerializeField] private float _velocityPower;
         private Rigidbody2D _rigidbody;
+
+        private const float EPSILON = 0.01f;
 
         private void Awake() => _rigidbody = GetComponent<Rigidbody2D>();
 
         public void Move(float direction)
         {
-            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x + direction * _speed * Time.fixedDeltaTime, _rigidbody.velocity.y);
+            float targetVelocity = direction * _maxSpeed;
+            float velocityDifference = targetVelocity - _rigidbody.velocity.x;
+            float accelerationRate = (Mathf.Abs(targetVelocity) > EPSILON) ? _acceleration : _decceleration;
+            float movementX = Mathf.Pow(Mathf.Abs(velocityDifference) * accelerationRate, _velocityPower) * Mathf.Sign(velocityDifference);
+
+            _rigidbody.AddForce(new Vector2(movementX, _rigidbody.velocity.y));
         }
     }
 }
