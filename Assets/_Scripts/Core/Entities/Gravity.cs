2f9067291391f56e087568a43cfb4f96bc5766e8
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-02-29 23:06:08 +0200

    Added minYVelocity parameter in Gravity.cs

--- Assets/_Scripts/Core/Entities/Gravity.cs
+++ Assets/_Scripts/Core/Entities/Gravity.cs
@@ -5,6 +5,7 @@ namespace KingOfGuns.Core.Entities
     public class Gravity : MonoBehaviour
     {
         [SerializeField] private float _force;
+        [SerializeField] private float _minYVelocity;
         private Rigidbody2D _rigidbody;
 
         private void Awake() => _rigidbody = GetComponent<Rigidbody2D>();
@@ -13,6 +14,12 @@ namespace KingOfGuns.Core.Entities
         {
             if (_rigidbody.velocity.y >= 0)
                 return;
+
+            if (_rigidbody.velocity.y < _minYVelocity)
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _minYVelocity);
+                return;
+            }
 
             _rigidbody.AddForce(Vector2.down * _force);
         }
